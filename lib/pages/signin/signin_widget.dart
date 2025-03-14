b6b020b8ca18b4a772aa0240d9accdcbb9df17ad
Gökhan Karaçay <gokhankaracay83@hotmail.com>
2025-03-14 15:10:16 +0100

    Sing In buttons added

--- lib/pages/signin/signin_widget.dart
+++ lib/pages/signin/signin_widget.dart
@@ -4,17 +4,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 Widget buildThirdPartyLogin(BuildContext context){
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 30.h),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
 
-    children: [
-      GestureDetector(
-        child: Container(
-          width: 40.w,
-          height: 40.w,
-          child: Image.asset("assets/icons/google.png"),
-        ),
-      ),
-    ],
+
+      children: [
+        socialButton("assets/icons/google.png"),
+        socialButton("assets/icons/apple.png"),
+        socialButton("assets/icons/facebook.png"),
+
+      ],
+    ),
+  );
+}
+
+Widget socialButton( String iconPath){
+  return GestureDetector(
+    child: Container(
+      width: 40.w,
+      height: 40.w,
+      child: Image.asset(iconPath),
+    ),
   );
 }
